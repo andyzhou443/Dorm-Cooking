5d919ac3dbb61f8ab4caf065cfae6c9f87dfb4f8
Andy Zhou <andyzhou443@gmail.com>
2025-04-17 01:37:15 -0400

    Added basic navigation bar, setup, page

---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify
+from flask import redirect, url_for
+app = Flask(__name__)
+
+Recipe = {
+    "1": {
+    },
+    "2": {
+    },
+    "3": {
+    },
+    "4": {
+    }
+
+}
+
+
+current_id = 4
+
+# ROUTES
+
+@app.route('/')
+def hello_world():
+   return render_template('hello_world.html')   
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+   app.run(debug = True, port=5001)
+
+
+
+
